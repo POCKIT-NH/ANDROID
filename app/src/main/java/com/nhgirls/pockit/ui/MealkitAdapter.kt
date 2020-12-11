7a5fd41cab3adb10eb233378cee59cce8b9f5606
RoadtoS7 <kshvv975@gmail.com>
2020-12-12 01:53:50 +0900

    feat(MealKitAdapter.kt): Add RecyclerView Adapter for CategoryDetailFragment

---
+++ app/src/main/java/com/nhgirls/pockit/ui/MealkitAdapter.kt
@@ -0,0 +1,52 @@
+package com.nhgirls.pockit.ui
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.nhgirls.pockit.data.Mealkit
+
+private const val FIRST_MEAL_INDEX = 1
+private const val FIRST_MEAL_VIEW_TYPE = 1
+private const val THE_REST_MEAL_VIEW_TYPE = 2
+
+class MealkitAdapter(private val onClickListener: OnClickListener) :
+    androidx.recyclerview.widget.ListAdapter<Mealkit, RecyclerView.ViewHolder>(DiffCallback) {
+    override fun getItemViewType(position: Int): Int {
+        return if (position == FIRST_MEAL_INDEX) {
+            FIRST_MEAL_VIEW_TYPE
+        } else {
+            THE_REST_MEAL_VIEW_TYPE
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == FIRST_MEAL_VIEW_TYPE) {
+            FirstMealkitViewHolder.from(parent)
+        } else {
+            TheRestMealkitViewHolder.from(parent)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder.itemViewType == FIRST_MEAL_VIEW_TYPE) {
+            (holder as FirstMealkitViewHolder).bind(getItem(position), onClickListener)
+        } else {
+            (holder as TheRestMealkitViewHolder).bind(getItem(position), onClickListener)
+        }
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Mealkit>() {
+        override fun areItemsTheSame(oldItem: Mealkit, newItem: Mealkit): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Mealkit, newItem: Mealkit): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    class OnClickListener(private val clickListener: (view: View, artist: Mealkit) -> Unit) {
+        fun onClick(view: View, artist: Mealkit) = clickListener(view, artist)
+    }
+}
