40168ba59860ba7863fdecdd1fedb3d4e8112f85
RoadtoS7 <kshvv975@gmail.com>
2020-12-12 23:45:34 +0900

    feat(TheRestMealkitViewHolder.kt): Add TheRestMealkitViewHolder with xml

--- app/src/main/java/com/nhgirls/pockit/ui/TheRestMealkitViewHolder.kt
+++ app/src/main/java/com/nhgirls/pockit/ui/TheRestMealkitViewHolder.kt
@@ -5,22 +5,30 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.nhgirls.pockit.data.Mealkit
 import com.nhgirls.pockit.databinding.ItemMealkitTherestBinding
+import com.nhgirls.pockit.utils.setRound
 import org.jetbrains.annotations.NotNull
 
 class TheRestMealkitViewHolder private constructor(private val binding: @NotNull ItemMealkitTherestBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(mealKit: Mealkit, onClickListener: MealkitAdapter.OnClickListener){
-
+        binding.mealKit = mealKit
     }
 
     companion object{
+        private const val CORNER_RADIUS = 9.5f
+        private const val IMAGE_CORNER_RADIUS = 9.0f
         fun from(viewGroup: ViewGroup): TheRestMealkitViewHolder{
             val binding = ItemMealkitTherestBinding.inflate(
                 LayoutInflater.from(viewGroup.context),
                 viewGroup,
                 false
             )
+            giveCornerRadius(binding)
             return TheRestMealkitViewHolder(binding)
+        }
+        private fun giveCornerRadius(binding: ItemMealkitTherestBinding) {
+            binding.root.setRound(CORNER_RADIUS)
+            binding.image.setRound(IMAGE_CORNER_RADIUS)
         }
     }
 }
