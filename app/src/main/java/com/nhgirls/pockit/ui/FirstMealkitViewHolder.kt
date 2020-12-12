ef659907fe9dfb3684231bc3c87c76e0fb233646
RoadtoS7 <kshvv975@gmail.com>
2020-12-12 23:45:04 +0900

    feat(FirstMealkitViewHolder.kt): Add FragmentMealkitViewholder with xml

--- app/src/main/java/com/nhgirls/pockit/ui/FirstMealkitViewHolder.kt
+++ app/src/main/java/com/nhgirls/pockit/ui/FirstMealkitViewHolder.kt
@@ -5,23 +5,32 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.nhgirls.pockit.data.Mealkit
 import com.nhgirls.pockit.databinding.ItemMealkitFirstBinding
+import com.nhgirls.pockit.utils.setRound
 
 import org.jetbrains.annotations.NotNull
 
 class FirstMealkitViewHolder private constructor(private val binding: @NotNull ItemMealkitFirstBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(mealKit: Mealkit, onClickListener: MealkitAdapter.OnClickListener){
-
+        binding.mealKit = mealKit
     }
 
     companion object{
+        private const val ROOT_CORNER_RADIUS = 9.5f
+        private const val IMAGE_CORNER_RADIUS = 9.0f
         fun from(viewGroup: ViewGroup): FirstMealkitViewHolder{
             val binding = ItemMealkitFirstBinding.inflate(
                 LayoutInflater.from(viewGroup.context),
                 viewGroup,
                 false
             )
+            giveCornerRadius(binding)
             return FirstMealkitViewHolder(binding)
+        }
+
+        private fun giveCornerRadius(binding: ItemMealkitFirstBinding) {
+            binding.root.setRound(ROOT_CORNER_RADIUS)
+            binding.image.setRound(IMAGE_CORNER_RADIUS)
         }
     }
 }
