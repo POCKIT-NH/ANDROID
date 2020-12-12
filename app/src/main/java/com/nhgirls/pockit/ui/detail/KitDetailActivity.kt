80058d12c2d0391ba46b473088d888d657554196
yanghyesun <nbn0809@naver.com>
2020-12-13 01:29:21 +0900

    feat(drawable): Add assets

---
+++ app/src/main/java/com/nhgirls/pockit/ui/detail/KitDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.nhgirls.pockit.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nhgirls.pockit.R
+
+class KitDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kit_detail)
+    }
+}
--- app/src/main/java/com/nhgirls/pockit/utils/RecyclerViewExt.kt
+++ app/src/main/java/com/nhgirls/pockit/utils/RecyclerViewExt.kt
@@ -1,4 +1,4 @@
-package com.pockit.android.util
+package com.nhgirls.pockit.utils
 
 import android.view.LayoutInflater
 import android.view.View
