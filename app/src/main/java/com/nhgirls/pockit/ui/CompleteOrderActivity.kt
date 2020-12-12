6f7a100bbad803d17db8b52ad5d986a7d251e875
RoadtoS7 <kshvv975@gmail.com>
2020-12-12 23:58:15 +0900

    feat(CompleteOrderActivity): Add CompleteOrderActivity wit xml

---
+++ app/src/main/java/com/nhgirls/pockit/ui/CompleteOrderActivity.kt
@@ -0,0 +1,12 @@
+package com.nhgirls.pockit.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nhgirls.pockit.R
+
+class CompleteOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complete_order)
+    }
+}
