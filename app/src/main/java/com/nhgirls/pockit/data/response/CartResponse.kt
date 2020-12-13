85d5a16ae0e0736a91a27acdb5fd0eb8ce4f80bf
RoadtoS7 <kshvv975@gmail.com>
2020-12-13 22:22:17 +0900

    feat(CartResponse.kt): Add CartResponse

---
+++ app/src/main/java/com/nhgirls/pockit/data/response/CartResponse.kt
@@ -0,0 +1,17 @@
+package com.nhgirls.pockit.data.response
+
+import com.google.gson.annotations.SerializedName
+import com.nhgirls.pockit.data.Cart
+
+data class CartResponse (
+    @SerializedName("user_idx")
+    val userId: Int,
+    @SerializedName("order_idx")
+    val orderId: Int,
+    @SerializedName("delivery_date")
+    val deliveryDate: String,
+    @SerializedName("delivery_price")
+    val deliveryPrice: Int,
+    @SerializedName("product")
+    val products: List<Cart>
+)
