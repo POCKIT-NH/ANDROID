b2ee97dbd651f9c2a334b89ceeea8a09962bb561
yanghyesun <nbn0809@naver.com>
2020-12-13 18:09:36 +0900

    feat(TodayPockitRecyclerAdapter.kt): Add TodayPockitRecyclerAdapter

--- app/src/main/java/com/nhgirls/pockit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nhgirls/pockit/ui/home/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.tabs.TabLayout
 import com.nhgirls.pockit.R
+import com.nhgirls.pockit.common.HorizontalItemDecorator
+import com.nhgirls.pockit.data.response.TodayPrice
 import kotlinx.android.synthetic.main.fragment_home.*
 
 private const val ARG_PARAM1 = "param1"
@@ -15,6 +17,14 @@ private const val ARG_PARAM2 = "param2"
 class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+
+    private var mock = listOf(
+        TodayPrice("https://sopt-server-27.s3.ap-northeast-2.amazonaws.com/4.jpg","장어",1000, 86),
+        TodayPrice("https://sopt-server-27.s3.ap-northeast-2.amazonaws.com/4.jpg","월계수잎",1000, 86),
+        TodayPrice("https://sopt-server-27.s3.ap-northeast-2.amazonaws.com/4.jpg","보쌈",1000, 86)
+    )
+
+    private lateinit var todayPockitAdapter: TodayPockitRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +46,16 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initViewPager()
+        initRecycler()
+    }
+
+    private fun initRecycler(){
+        todayPockitAdapter = TodayPockitRecyclerAdapter()
+        rvTodayPockit.apply{
+            adapter = todayPockitAdapter
+            addItemDecoration(HorizontalItemDecorator(24))
+        }
+        todayPockitAdapter.initData(mock)
     }
 
     private fun initViewPager(){
@@ -43,9 +63,6 @@ class HomeFragment : Fragment() {
 
         vpTodayPrice.offscreenPageLimit = 3
         tlTodayPrice.setViewPager(vpTodayPrice)
-
-//        vpTodayPrice.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tlTodayPrice))
-//        tlTodayPrice.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(vpTodayPrice))
     }
 
     companion object {
