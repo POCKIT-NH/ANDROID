d3ae0ee66240435f20773e57ca34667bed78721e
yanghyesun <nbn0809@naver.com>
2020-12-13 12:30:31 +0900

    feat(HomeFragment.kt): Add HomeFragment Today specailly Price

--- app/src/main/java/com/nhgirls/pockit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nhgirls/pockit/ui/home/HomeFragment.kt
@@ -5,20 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayout
 import com.nhgirls.pockit.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -38,16 +32,24 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViewPager()
+    }
+
+    private fun initViewPager(){
+        vpTodayPrice.adapter = HomeViewPagerAdapter()
+
+        vpTodayPrice.offscreenPageLimit = 3
+        tlTodayPrice.setViewPager(vpTodayPrice)
+
+//        vpTodayPrice.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tlTodayPrice))
+//        tlTodayPrice.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(vpTodayPrice))
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {
