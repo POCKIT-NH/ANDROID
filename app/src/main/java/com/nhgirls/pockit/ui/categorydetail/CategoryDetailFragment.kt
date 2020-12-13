70552aa19bf3cafe403b3cc3143acfdae3adb81a
RoadtoS7 <kshvv975@gmail.com>
2020-12-13 18:33:24 +0900

    feat(CategoryDetailFragment.kt): Rename mealkit_list, MealkititemDecoration
    mealkit_list를 cart_list로, MealkititemDecoration를 MealKitItemDecoration으로 바꿈

--- app/src/main/java/com/nhgirls/pockit/ui/categorydetail/CategoryDetailFragment.kt
+++ app/src/main/java/com/nhgirls/pockit/ui/categorydetail/CategoryDetailFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.nhgirls.pockit.R
 import com.nhgirls.pockit.data.DummyContainer
-import com.nhgirls.pockit.ui.MealkititemDecoration
 import com.nhgirls.pockit.utils.dpToPx
 import kotlinx.android.synthetic.main.fragment_category_detail.view.*
 
@@ -24,9 +23,9 @@ class CategoryDetailFragment : Fragment() {
         val mealkitAdapter = MealkitAdapter(MealkitAdapter.OnClickListener {
             // todo 리사이클러뷰 아이템 클릭시 클릭 이벤트 처리하기
         })
-        view.mealkit_list.adapter = mealkitAdapter
-        view.mealkit_list.addItemDecoration(
-            MealkititemDecoration(
+        view.cart_list.adapter = mealkitAdapter
+        view.cart_list.addItemDecoration(
+            MealkitItemDecoration(
                 MEALKIT_LIST_SPANCOUNT,
                 MEALKIT_LIST_SPACING
             )
