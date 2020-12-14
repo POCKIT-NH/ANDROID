e41f9ab13f07c3feadc1dd6a531d5f55083e0bf2
yanghyesun <nbn0809@naver.com>
2020-12-14 13:15:08 +0900

    feature(StoryDetailActivity.kt) : Add StoryDetailActivity.kt

--- app/src/main/java/com/nhgirls/pockit/ui/story/StoryRecyclerAdapter.kt
+++ app/src/main/java/com/nhgirls/pockit/ui/story/StoryRecyclerAdapter.kt
@@ -1,12 +1,15 @@
 package com.nhgirls.pockit.ui.story
 
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.nhgirls.pockit.PockitApplication
 import com.nhgirls.pockit.R
 import com.nhgirls.pockit.common.GlideApp
+import com.nhgirls.pockit.common.setOnDebounceClickListener
 import com.nhgirls.pockit.data.StoryItem
 import com.nhgirls.pockit.utils.dpToPx
 import com.nhgirls.pockit.utils.inflate
@@ -47,6 +50,13 @@ class StoryRecyclerAdapter : RecyclerView.Adapter<StoryRecyclerAdapter.ViewHolde
             txtStoryTitle2.text = item.title2
             txtStoryDate.text = item.date
             txtStoryVisit.text = "${item.visit}"
+
+            itemView.setOnDebounceClickListener {
+                Intent(
+                    PockitApplication.GlobalApp, StoryDetailActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }.run { PockitApplication.GlobalApp.startActivity(this) }
+            }
         }
     }
 }
