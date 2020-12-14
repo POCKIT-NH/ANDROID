717b5659f629dc1a4e57de9624827aea3cec4016
RoadtoS7 <kshvv975@gmail.com>
2020-12-14 13:22:26 +0900

    feat(ForestViewModel.kt): Add ForestViewModel

---
+++ app/src/main/java/com/nhgirls/pockit/ui/forest/ForestViewModel.kt
@@ -0,0 +1,21 @@
+package com.nhgirls.pockit.ui.forest
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.nhgirls.pockit.api.Service
+import com.nhgirls.pockit.utils.safeEnqueue
+
+class ForestViewModel : ViewModel() {
+    private val _forestPercent = MutableLiveData<Int>()
+    val forestPercent: LiveData<Int>
+        get() = _forestPercent
+
+    init {
+        Service.service.getForest().safeEnqueue(
+            {},
+            { _forestPercent.value = it.data },
+            {}
+        )
+    }
+}
